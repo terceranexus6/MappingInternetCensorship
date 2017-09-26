b088f72afe807e4ac98e4d44eefd63af99eead9b
Paula <pauladelahoz@correo.ugr.es>
2017-09-26 08:40:00 -0400

    añadir index

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route("/")
+
+
 def main():
-    return "Welcome!"
+    return render_template('index.html')
+
 if __name__ == "__main__":
     app.run()
